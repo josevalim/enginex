0add1a8c421f74912e1b4b55ab9553393810e1e2
José Valim <jose.valim@gmail.com>
2010-04-23 12:39:21 +0200

    Create ActiveSupport::IntegrationCase for Capybara.

--- lib/enginex.rb
+++ lib/enginex.rb
@@ -7,7 +7,7 @@ require "rails/generators"
 require "rails/generators/rails/app/app_generator"
 
 class Enginex < Thor::Group
-  VERSION = "0.5.4".freeze
+  VERSION = "0.6.0".freeze
 
   include Thor::Actions
   check_unknown_options!
